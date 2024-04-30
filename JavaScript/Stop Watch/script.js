5327141c8a54b2248a5c786633a775ccd04fdcd1
Abhinandan411 <abhinandankumar6204@gmail.com>
2024-04-30 18:15:18 +0530

    added stop watch assignmnet

---
+++ JavaScript/Stop Watch/script.js
@@ -0,0 +1,61 @@
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+
+
+
+
+
+document.getElementById("statrbtn").addEventListener(('click'), start)
+function start() {
+    if (!isRunning) {
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(updateDisplay, 10);
+        console.log(timer)
+        isRunning = true;
+    }
+    //    console.log(startTime)
+}
+
+
+document.getElementById("stopbtn").addEventListener(('click'), stop)
+function stop() {
+    if (isRunning) {
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;
+    }
+}
+
+
+document.getElementById("resetbtn").addEventListener(('click'), reset)
+function reset() {
+    clearInterval(timer);
+    timer = null;
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00"
+
+
+}
+
+function updateDisplay() {
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let miliSeconds = Math.floor((elapsedTime % 1000) / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    miliSeconds = String(miliSeconds).padStart(2, "0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${miliSeconds}`
+}
