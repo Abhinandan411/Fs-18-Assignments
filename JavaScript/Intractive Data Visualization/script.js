3a5ca3a45c0a1b5fda164d7b2e2f10030a8c872b
Abhinandan411 <abhinandankumar6204@gmail.com>
2024-05-07 19:08:13 +0530

    added Intractive chart visualization assignment

--- JavaScript/Intractive Data Visualization/script.js
+++ JavaScript/Intractive Data Visualization/script.js
@@ -1,14 +1,15 @@
 const myChart = new Chart("myChart", {
-    type: "bar",
-    data : {
-        labels: ["Jan" , "Feb" , "March" , "April" , "May"],
-        datasets: [{
+  type: "bar",
+  data: {
+      labels: ["January", "February", "March", "April", "May"],
+      datasets: [{
           label: 'My First Dataset',
           data: [65, 59, 80, 81, 56, 55, 40],
           fill: false,
-          borderColor: 'rgb(75, 192, 192)',
+          borderColor: 'rgb(158, 194, 194)',
+          backgroundColor: 'rgba(117, 216, 216, 0.5)', // Change the color of the bars here
           tension: 0.1
-        }]
-      },
-    options: {}
+      }]
+  },
+  options: {}
 });
